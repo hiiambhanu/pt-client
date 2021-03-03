dba6df31aaf4f5288b29cd608468dc740514b1a0
Bhanu Pratap Singh <hiiambhanu@gmail.com>
2021-03-03 11:15:43 +0530

    Updates

---
+++ precache-manifest.8ea31725695dc3934bf3633fc65f1524.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb170238ebe8fa5eaac3e2555624d306",
+    "url": "/pt-client/index.html"
+  },
+  {
+    "revision": "e7e48257d3c25b128f79",
+    "url": "/pt-client/static/css/main.451cea61.chunk.css"
+  },
+  {
+    "revision": "59cb1fd9ca66ba6001ff",
+    "url": "/pt-client/static/js/2.10102028.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pt-client/static/js/2.10102028.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7e48257d3c25b128f79",
+    "url": "/pt-client/static/js/main.8ab288ce.chunk.js"
+  },
+  {
+    "revision": "b9909a19d6532e806642",
+    "url": "/pt-client/static/js/runtime-main.2ab98fe4.js"
+  }
+]);
